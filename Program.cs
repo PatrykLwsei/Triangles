1a215b51495ddc3238e1dd3931de667f081a72c7
PatrykLwsei <115666451+PatrykLwsei@users.noreply.github.com>
2022-11-27 20:16:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Zadanie_ParametryTrojkata
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+        // wczytaj dane
+        // i wykonaj obliczenia
+    }
+  }
+}
